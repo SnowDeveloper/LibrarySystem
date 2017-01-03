4ac442a27cdd15adf9cac7f6f6ec393ac2732321
Madhushree Singh <contact.madhushreesingh@gmail.com>
2017-01-03 16:07:51 +0200

    Database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+more_users = User.create([{username: 'user1'}, {username: 'user2'}])
+more_books = [{title: 'The Ruby way', authors: 'testauthor', call_no: '1'},
+                          {title: 'Secrets of the JavaScript Ninja', authors: 'testauthor2', call_no: '2'}]
+
+more_loans = [{start_date: '09-01-2016', return_date: '23-01-2016', cost: 1.5},
+              {start_date: '04-01-2016', return_date: '16-01-2016', cost: 2.5}]
+
+more_loans.each do |loan|
+  new_loan = Loan.new({:start_date => loan[:start_date], :return_date => loan[:return_date]})
+  new_loan.user = more_users.last
+  new_loan.book = Book.create(more_books.pop)
+  new_loan.save!
+end
